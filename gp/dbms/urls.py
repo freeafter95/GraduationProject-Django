4b54f26deccd651a2616beb000d050ad8a170cda
root <18504510214@163.com>
2019-04-07 21:06:53 +0800

    Init local project

---
+++ gp/dbms/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+  path('',views.login, name ='login'),
+  path('login/',views.login, name='login'),
+  path('regist/',views.regist, name='regist'),
+  path('index/',views.index,name = 'index'),
+  path('logout/',views.logout,name = 'logout'),
+   path('test/',views.test,name = 'test'),
+]
+
