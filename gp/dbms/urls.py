bb893fe56782b86ec22c137e8b397c1e9024d616
Lijidarkx <18504510214@163.com>
2019-04-09 14:38:34 +0800

    Update

--- gp/dbms/urls.py
+++ gp/dbms/urls.py
@@ -3,9 +3,9 @@ from . import views
 urlpatterns = [
   path('',views.login, name ='login'),
   path('login/',views.login, name='login'),
-  path('regist/',views.regist, name='regist'),
-  path('index/',views.index,name = 'index'),
-  path('logout/',views.logout,name = 'logout'),
-  path('test/',views.test,name = 'test'),
+  # path('regist/',views.regist, name='regist'),
+  # path('index/',views.index,name = 'index'),
+  # path('logout/',views.logout,name = 'logout'),
+  # path('test/',views.test,name = 'test'),
 ]
 
