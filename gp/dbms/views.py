45ebcce5c340e20a2d09af8a84269e8f9cca8126
Lijidarkx <18504510214@163.com>
2019-04-09 14:55:29 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -21,7 +21,7 @@ def login(request):
     print("session:",request.session.session_key)
     random_filename = "".join(random.sample(string.ascii_lowercase,4))
     random_code = generate_code.gene_code(verify_path,random_filename)
-    cache.set(random_filename, random_code, ttl=30)
+    cache.set(cache, random_filename, random_code, ttl=30)
 
     if request.method == 'POST':
 
