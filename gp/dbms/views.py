05abf8f7e8cedd813b0d383fb4a108dfe9facd21
Lijidarkx <18504510214@163.com>
2019-04-12 13:16:57 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -16,7 +16,7 @@ cache = Cache()
 def check_login(func_type='func'):
     def decorator(func):
         def wrapper(*args):
-            if func_type == 'class' and args[1].COOKIES.get('username') or
+            if (func_type == 'class' and args[1].COOKIES.get('username')) or \
             (func_type == 'func' and args[0].COOKIES.get('username')):
                 return redirect('/dbms/mainterface/')
             return func(*args)
