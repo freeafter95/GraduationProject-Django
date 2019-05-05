ad4e8c848930c292e2efa2c60cfa1226b67fdae5
Lijidarkx <18504510214@163.com>
2019-05-05 22:10:00 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -18,12 +18,11 @@ def check_login(per_type='3', func_type='func'):
     def decorator(func):
         def wrapper(*args):
             print(args)
-            try:
             if (func_type == 'class' and args[1].session.get('username') is None) or \
             (func_type == 'func' and args[0].session.get('username') is None):
                 return redirect('/dbms/login/')
             if ((func_type == 'class' and (args[1].session.get('permission') is None or int(per_type) < int(args[1].session.get('permission')))) or \
-                (func_type == 'func' and (args[1].session.get('permission') is None or int(per_type) < int(args[0].session.get('permission'))))):
+            (func_type == 'func' and (args[1].session.get('permission') is None or int(per_type) < int(args[0].session.get('permission'))))):
                 return redirect('/dbms/mainterface/')
             return func(*args)
         return wrapper
