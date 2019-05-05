4d2271b58a76b2bfd0e95d51b13c0bc1dd484535
Lijidarkx <18504510214@163.com>
2019-05-05 21:27:30 +0800

    Update

--- gp/dbms/models.py
+++ gp/dbms/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class UserInfo(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    permission = models.CharField(max_length=1)
+# class UserInfo(models.Model):
+#     username = models.CharField(max_length=20)
+#     password = models.CharField(max_length=20)
+#     permission = models.CharField(max_length=1)
 
 
