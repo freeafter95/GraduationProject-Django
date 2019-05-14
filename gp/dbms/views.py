b84273a5df4be3caa3faa56c1e76b0c884746783
Lijidarkx <18504510214@163.com>
2019-05-14 15:31:47 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -211,9 +211,10 @@ def crystal_query(request):
                 if v.strip() != '':
                     select_fields.add(k[0:-1])
                     if k == 'second_elem1':
-                        select_conditions['second_elem__contains'] = '%'.join(v.strip().split(' '))
+                        select_conditions['second_elem__icontains'] = '%'.join(v.strip().split(' '))
+                        print(models.Djbasicnatu.objects.filter(second_elem__icontains=select_conditions['second_elem__icontains']).first().values('id'))
                     else:
-                        select_conditions[k[0:-1] + '__contains'] = v.strip()
+                        select_conditions[k[0:-1] + '__icontains'] = v.strip()
             else:
                 select_fields.add(k)
 
@@ -252,7 +253,7 @@ def user_manage(request):
     if request.method == 'GET':
         select_users = models.UserInfo.objects.all()
     else:
-        select_users = models.UserInfo.objects.filter(username__contains=request.POST.get('usersearch'))
+        select_users = models.UserInfo.objects.filter(username__icontains=request.POST.get('usersearch'))
     users = [{'name': user.username, 'permission': user.permission} for user in select_users]
     print(type(users))
     return render(request, 'usermanage.html', {'users': users})
