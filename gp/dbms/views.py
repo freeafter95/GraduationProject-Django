81634837532e30e8c956642786b37b2fe9c9643b
Lijidarkx <18504510214@163.com>
2019-05-31 15:55:52 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -146,6 +146,28 @@ input_lists = {
     }
 }
 
+def check_type(input_info, content, ret_dic):
+    if input_info[1] == 'float':
+        try:
+            return float(content)
+        except ValueError:
+            if 'error' in ret_dic:
+                ret_dic['error'] += '%s应该为浮点型\n' % input_info[0]
+            else:
+                ret_dic['error'] = '%s应该为浮点型\n' % input_info[0]
+            return ''
+    elif input_info[1] == 'int':
+        try:
+            return int(content)
+        except ValueError:
+            if 'error' in ret_dic:
+                ret_dic['error'] += '%s应该为整型\n' % input_info[0]
+            else:
+                ret_dic['error'] = '%s应该为整型\n' % input_info[0]
+            return ''
+
+    return content
+
 def del_session(request):
     try:
         for k in request.session.keys():
@@ -285,19 +307,19 @@ def crystal_insert(request):
         input_dic = {}
         ret_dic = {}
         for attr in input_lists['crystal_list'].keys():
-            if attr == 'insert_time':
-                continue
             content = request.POST.get(attr).strip()
             if content is not None and content != '':
-                input_dic[attr] = content
+                input_dic[attr] = check_type()
 
         if (input_dic.get('main_elem') is not None \
         and input_dic.get('second_elem') is not None) \
         or input_dic.get('alloy_grade') is not None:
-            models.Djbasicnatu.objects.create(**input_dic)
-            ret_dic['success'] = '添加成功'
+            if len(ret_dic) == 0:
+                models.Djbasicnatu.objects.create(**input_dic)
+                ret_dic['success'] = '添加成功'
         else:
-            ret_dic['error'] = '合金牌号或主元素与次元素必须填写'
+            ret_dic['error'] += '合金牌号或主元素与次元素必须填写\n'
+
         return render(request, 'crystalinsert.html', ret_dic)
 
 @check_login('crystalquery')
