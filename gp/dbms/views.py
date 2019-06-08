485a08d696fd431b4cf4134abf5be5016a54c5d8
Lijidarkx <18504510214@163.com>
2019-06-08 21:54:06 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -774,7 +774,7 @@ def radiation_allout(request):
 
 @check_login('')
 def show_pic(request, id):
-    path = models.Picture.objects.filter(id=id).first()[0]['pic_path']
+    path = models.Picture.objects.get(id=id)[0]['pic_path']
     with open(path, 'rb') as file:
         pic = file.read()
     return HttpResponse(pic, content_type='image/jpeg')
