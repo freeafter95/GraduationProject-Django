5b8159c6fde9c638e18ef9e716d891144ec2b732
Lijidarkx <18504510214@163.com>
2019-06-20 14:56:17 +0800

    Update

--- gp/dbms/views.py
+++ gp/dbms/views.py
@@ -247,12 +247,6 @@ def check_login(cookie_name, per_type='3', func_type='func'):
             if int(per_type) < int(req.session.get('permission')):
                 print('No permission!')
                 return redirect('/dbms/mainterface/')
-            # if (func_type == 'class' and args[1].session.get('username') is None) or \
-            # (func_type == 'func' and args[0].session.get('username') is None):
-            #     return redirect('/dbms/login/')
-            # if ((func_type == 'class' and (args[1].session.get('permission') is None or int(per_type) < int(args[1].session.get('permission')))) or \
-            # (func_type == 'func' and (args[0].session.get('permission') is None or int(per_type) < int(args[0].session.get('permission'))))):
-            #     return redirect('/dbms/mainterface/')
             res = func(*args, **kwargs)
             if cookie_name != '' and 'query' not in cookie_name:
                 res.set_cookie('current_page', cookie_name)
@@ -572,6 +566,10 @@ def all_allin(request, table):
                                 l = models.Literature.objects.filter(serial_num=input_list['literature']).first()
                                 if l:
                                     input_dic['literature'] = l
+                            if 'picture' in input_dic.keys():
+                                l = models.Literature.objects.filter(id=input_list['picture']).first()
+                                if l:
+                                    input_dic['picture'] = l
                             
                             mc.objects.create(**input_dic)
                             success_count += 1
@@ -607,6 +605,14 @@ def all_allin(request, table):
                 if file_type == 'json':
                     try:
                         input_dic = json.loads(last)
+                        if 'literature' in input_dic.keys():
+                            l = models.Literature.objects.filter(serial_num=input_list['literature']).first()
+                            if l:
+                                input_dic['literature'] = l
+                        if 'picture' in input_dic.keys():
+                            l = models.Literature.objects.filter(id=input_list['picture']).first()
+                            if l:
+                                input_dic['picture'] = l
                         mc.objects.create(**input_dic)
                         success_count += 1
                     except TypeError:
@@ -618,7 +624,14 @@ def all_allin(request, table):
                     input_list = last.split(',')
                     ret_dic = {'errors': []}
                     for i in range(len(input_list)):
-                        input_dic[header[i]] = check_type(input_lists[table + '_list'][header[i]], input_list[i], ret_dic)
+                        if header[i] == 'literature':
+                            l = models.Literature.objects.filter(serial_num=input_list[i]).first()
+                            if l:
+                                input_dic[header[i]] = l
+                            else:
+                                ret_dic['errors'].append('文献编号在数据库中未查询到')
+                        else:
+                            input_dic[header[i]] = check_type(input_lists[table + '_list'][header[i]], input_list[i], ret_dic)
                     if len(ret_dic['errors']) == 0:
                         mc.objects.create(**input_dic)
                         success_count += 1
@@ -925,96 +938,3 @@ def add_user(request):
 def del_user(request, username):
     models.UserInfo.objects.filter(username=username).delete()
     return user_manage(request)
-
-
-# cache = Cache()
-# VERIFY_IMG_DIR = os.path.dirname(os.path.dirname(__file__)) + '/static/verify_code'
-
-# def login(request):
-#     error_msg = ''
-#     today_str = datetime.date.today().strftime("%Y%m%d")
-#     verify_path = "%s/%s" % (VERIFY_IMG_DIR, today_str)
-#     if not os.path.isdir(verify_path):
-#         os.makedirs(verify_path, exist_ok=True)
-#     print("session:",request.session.session_key)
-#     random_filename = "".join(random.sample(string.ascii_lowercase,4))
-#     random_code = generate_code.gene_code(verify_path,random_filename)
-#     cache.set(random_filename, random_code, ttl=30)
-
-#     if request.method == 'POST':
-
-#         _verify_code = request.POST.get('verify_code')
-#         _verify_code_key  = request.POST.get('verify_code_key')
-#         print(cache.get(_verify_code_key))
-#         print(_verify_code)
-#         if cache.get(_verify_code_key) is not None and cache.get(_verify_code_key).lower() == _verify_code.lower():
-#             print("code verification pass!")
-#         else:
-#             error_msg = "验证码错误!"
-
-#     return render(request,'login.html',{"filename":random_filename, "today_str":today_str, "error":error_msg})
-
-
-
-
-# from django import forms
-# from dbms.models import User
-
-# #表单
-# class UserForm(forms.Form):
-#     username = forms.CharField(label='用户名',max_length=100)
-#     password = forms.CharField(label='密码',widget=forms.PasswordInput())
-
-
-#注册
-# def regist(req):
-#     if req.method == 'POST':
-#         uf = UserForm(req.POST)
-#         if uf.is_valid():
-#             #获得表单数据
-#             username = uf.cleaned_data['username']
-#             password = uf.cleaned_data['password']
-#             #添加到数据库
-#             User.objects.create(username= username,password=password)
-#             return HttpResponse('regist success!!')
-#     else:
-#         uf = UserForm()
-#     return render_to_response('regist.html',{'uf':uf}, )
-
-# #登陆
-# def login(req):
-#     if req.method == 'POST':
-#         uf = UserForm(req.POST)
-#         if uf.is_valid():
-#             #获取表单用户密码
-#             username = uf.cleaned_data['username']
-#             password = uf.cleaned_data['password']
-#             #获取的表单数据与数据库进行比较
-#             user = User.objects.filter(username__exact = username,password__exact = password)
-#             if user:
-#                 #比较成功，跳转index
-#                 response = HttpResponseRedirect('/dbms/index/')
-#                 #将username写入浏览器cookie,失效时间为3600
-#                 response.set_cookie('username',username,3600)
-#                 return response
-#             else:
-#                 #比较失败，还在login
-#                 return HttpResponseRedirect('/dbms/login/')
-#     else:
-#         uf = UserForm()
-#     return render_to_response('login.html',{'uf':uf}, )
-
-# def test(req):
-#     return render_to_response('test.html', {} )
-
-# #登陆成功
-# def index(req):
-#     username = req.COOKIES.get('username','')
-#     return render_to_response('index.html' ,{'username':username})
-
-# #退出
-# def logout(req):
-#     response = HttpResponse('logout !!')
-#     #清理cookie里保存username
-#     response.delete_cookie('username')
-#     return response
